05650eba76720ebe969cd33332026c49b062183d
dempseys3 <54369510+dempseys3@users.noreply.github.com>
2019-08-21 12:19:02 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*Hello.java
+*Hello.java/ print hello world
+*Sara Dempsey
+*08/21/2019
+*Cmsc 255 section 4*/
+
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Your Name");
+		System.out.println("Java Project Name");
+		System.out.println("Project Description");
+		System.out.println("Version Date");
+		System.out.println("Course Number and Section");
+		System.out.println();
+	}
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
